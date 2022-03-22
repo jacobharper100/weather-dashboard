74bee1c0788dd1003d00dd0169495a6037f16681
jacobharper100 <jacobharper100@gmail.com>
2022-03-22 15:09:48 -0400

    Added boilerplate React client frontend

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <p>
+            Hello, world!
+        </p>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    "mode": "development",
+    "entry": "./src/client/index.js",
+    "output": {
+        "path": __dirname + '/src/build',
+        "filename": "bundle.js"
+    },
+    "devtool": "source-map",
+    "module": {
+        "rules": [
+            {
+                "enforce": "pre",
+                "test": /\.js$/,
+                "exclude": /node_modules/,
+                "loader": "eslint-loader",
+                "options": {
+                  "emitWarning": true,
+                  "failOnError": false,
+                  "failOnWarning": false
+                }
+            },
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader"
+                }
+            },
+            {
+                "test": /\.css$/,
+                "use": [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                "test": /\.html$/,
+                "use": [
+                    {
+                        "loader": "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    "plugins": [
+        new HtmlWebPackPlugin({
+            template: "./src/client/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
