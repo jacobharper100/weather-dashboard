132660aa3c50bd713e8e0b341f273b0397941bc1
jacobharper100 <jacobharper100@gmail.com>
2022-03-22 16:03:17 -0400

    Configured /api proxy

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,52 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    "mode": "development",
-    "entry": "./src/client/index.js",
-    "output": {
-        "path": __dirname + '/build',
-        "filename": "bundle.js"
+    mode: "development",
+    entry: "./src/client/index.js",
+    output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
     },
-    "devtool": "source-map",
-    "module": {
-        "rules": [
+    devServer: {
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                pathRewrite: { '^/api': '' },
+            }
+        }
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
             {
-                "enforce": "pre",
-                "test": /\.js$/,
-                "exclude": /node_modules/,
-                "loader": "eslint-loader",
-                "options": {
-                  "emitWarning": true,
-                  "failOnError": false,
-                  "failOnWarning": false
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+                options: {
+                  emitWarning: true,
+                  failOnError: false,
+                  failOnWarning: false
                 }
             },
             {
-                "test": /\.(js|jsx)$/,
-                "exclude": /node_modules/,
-                "use": {
-                    "loader": "babel-loader"
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
                 }
             },
             {
-                "test": /\.css$/,
-                "use": [
+                test: /\.css$/,
+                use: [
                     "style-loader",
                     "css-loader"
                 ]
             },
             {
-                "test": /\.html$/,
-                "use": [
+                test: /\.html$/,
+                use: [
                     {
                         "loader": "html-loader"
                     }
@@ -45,7 +54,7 @@ module.exports = {
             }
         ]
     },
-    "plugins": [
+    plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/index.html",
             filename: "./index.html"
